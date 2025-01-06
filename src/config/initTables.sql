6749faf6dd83603d831fd5dbcfdeb015ae467aed
HtunThiha <htunthiha.dev@gmail.com>
2025-01-06 19:39:04 +0800

    New table user_challenge_completions, col rename
    user_challenge_completions is for records of completions whenever users have finished a challenge. column skillpoints_increase has been renamed to skillpoints in challenges table

--- src/config/initTables.sql
+++ src/config/initTables.sql
@@ -2,6 +2,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS challenges;
+DROP TABLE IF EXISTS user_challenge_completions;
 DROP TABLE IF EXISTS user_challenge_frequency;
 DROP TABLE IF EXISTS user_inbox;
 
@@ -20,10 +21,19 @@ CREATE TABLE challenges(
     challenge_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     creator_id INT NOT NULL,
     challenge VARCHAR(255),
-    skillpoints_increase INT NOT NULL,
+    skillpoints INT NOT NULL,
     is_active BOOLEAN DEFAULT 0,
     votes INT DEFAULT 0,
     FOREIGN KEY(creator_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE user_challenge_completions(
+    completion_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    challenge_id INT NOT NULL,
+    completed_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(user_id) REFERENCES users(user_id),
+    FOREIGN KEY(challenge_id) REFERENCES challenges(challenge_id)
 );
 
 CREATE TABLE user_challenge_frequency(
@@ -51,7 +61,7 @@ VALUES
     ("Steve", "Helloworld2277"),
     ("Sally", "LifeisSpice09");
 
-INSERT INTO challenges(creator_id, challenge, skillpoints_increase, is_active)
+INSERT INTO challenges(creator_id, challenge, skillpoints, is_active)
 VALUES
     (1, "Complete 2.4km within 15 minutes", 50, 1),
     (1, "Cycle around the island for at least 50km", 100, 1),
