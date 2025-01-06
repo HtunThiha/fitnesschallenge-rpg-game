d953a14f9181e39d513660134b8d449b146ce65b
HtunThiha <htunthiha.dev@gmail.com>
2025-01-06 16:04:20 +0800

    dateFormatter adjustments
    Now dateFormatter can format in accordance with the difference in days to the current date.
    date is today -> 12:00 AM
    date is in last week -> Sun 12:00 AM
    date is older than last week -> Jan 01 2024, 12:00AM

--- src/controllers/dateFormatter.js
+++ src/controllers/dateFormatter.js
@@ -1,17 +1,33 @@
 function formatDate(input_datestring) {
     const date = new Date(input_datestring);
+    const now = new Date();
+
+    const oneDay = 1000 * 60 * 60 * 24;
+    const differenceInDays = Math.floor((now - date) / oneDay);
 
     const options = {
-        day: 'numeric',
-        month: 'short',
-        year: '2-digit',
         hour: 'numeric',
         minute: '2-digit',
         hour12: true
-    };
+    }
+
+    if (differenceInDays == 0) {
+
+    } else if (differenceInDays > 0 && differenceInDays <= 7) {
+        options.weekday = 'short';
+    } else {
+        options.day = 'numeric';
+        options.month = 'short',
+        options.year = 'numeric';
+    }
+
+    let formattedDate = date.toLocaleString('en-US', options);
+
+    if (formattedDate.length > 12) {
+        formattedDate = formattedDate.replace(",", "");
+    }
     
-    const formattedDate = date.toLocaleString('en-US', options);
     return formattedDate;
 }
 
-module.exports = {formatDate};
+module.exports = { formatDate };
