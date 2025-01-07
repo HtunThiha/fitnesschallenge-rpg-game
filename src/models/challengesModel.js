3719e5037faecf7f74066f121c34af2015419b7d
HtunThiha <htunthiha.dev@gmail.com>
2025-01-07 11:41:27 +0800

    GET /challenges SQL update
    SQL statement has been updated to use INNER JOIN

--- src/models/challengesModel.js
+++ src/models/challengesModel.js
@@ -4,8 +4,9 @@ module.exports.selectAllChallenges = (callback) => {
 
     const SQLSTATEMENT = `
         SELECT c.challenge_id, u.username AS challenge_creator, c.challenge, c.skillpoints
-        FROM challenges AS c, users AS u
-        WHERE c.creator_id = u.user_id;
+        FROM challenges AS c
+        INNER JOIN users AS u
+        ON c.creator_id = u.user_id;
     `;
 
     pool.query(SQLSTATEMENT, callback);
